64855aa6c7ba747f066b6aeb12daf164980e54f2
serega-svarka <98324378+serega-svarka@users.noreply.github.com>
2023-10-17 13:39:55 +0600

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -11,27 +11,16 @@ contract BadgeToken is ERC721 {
     ) ERC721(_name, _symbol) {
     }
 
-    /**
-     * @dev Mints a token to an address with a tokenURI.
-     * @param _to address of the future owner of the token
-     */
     function mintTo(address _to) public {
         uint256 newTokenId = _getNextTokenId();
         _mint(_to, newTokenId);
         _incrementTokenId();
     }
 
-    /**
-     * @dev calculates the next token ID based on value of _currentTokenId
-     * @return uint256 for the next token ID
-     */
     function _getNextTokenId() private view returns (uint256) {
         return _currentTokenId+1;
     }
 
-    /**
-     * @dev increments the value of _currentTokenId
-     */
     function _incrementTokenId() private {
         _currentTokenId++;
     }
